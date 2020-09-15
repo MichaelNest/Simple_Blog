341ecb41ce63b931a0d7a2eadeb186391e3f01ac
MichaelNest <nesterenkom385@gmail.com>
2020-09-15 16:53:21 +0300

    Add create_date field in blog/models.py, add recording in class Meta; add recording post.create_date in blog/blog_detail.html.

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class Post(models.Model):
     title = models.CharField(max_length=222)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    create_date = models.DateTimeField(default=datetime.now)
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[str(self.id)])
@@ -13,5 +15,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    # class Meta:
-    #     ordering = ('-title',)
+    class Meta:
+        ordering = ('-create_date',)
